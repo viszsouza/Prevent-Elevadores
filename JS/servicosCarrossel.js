d58499ed541a48d0a1530d20b1c78844d73654b9
Caio Montenegro <caiombmonteiro@gmail.com>
2025-03-26 01:48:22 -0300

    Adição da responsividade da Main de Serviços - Empreendimentos
    - Troquei o nome do id do container do carrossel de Serviços, mas depois irei remover os id de cada container do Carrossel que eu colocar;
    - Corrige a media query no servicoCarrossel.js que antes estava com valor 459 ao invés de 390;
    - Aumentei o font-size do título do card.

--- JS/servicosCarrossel.js
+++ JS/servicosCarrossel.js
@@ -1,11 +1,11 @@
-// Variável - Media query
+// Variável para Media query
 var larguraTela = window.innerWidth;
 
 
 // Carrossel Serviços
 function direcaoServicos(e){
-    // Variável - Direção dos cards do carrossel
-    var direcao = document.getElementById("contentSlide");
+    // Variável para direção dos cards do carrossel
+    var direcao = document.getElementById("contentSlideResidenciais");
 
 
     // Media query ... - 398px
@@ -21,7 +21,62 @@ function direcaoServicos(e){
     }
 
     // Media query 390px - 458px
-    if(larguraTela >= 459 && larguraTela <= 458){
+    if(larguraTela >= 390 && larguraTela <= 458){
+        // Mover pra direita
+        if (e == 1){
+            direcao.scrollLeft = direcao.scrollLeft - 280;
+        }
+        // Mover pra direita
+        else if(e == 2){
+            direcao.scrollLeft = direcao.scrollLeft + 280;
+        };
+    }
+    
+    // Media query 459px - 598px
+    if(larguraTela >= 459 && larguraTela <= 598){
+        // Mover pra direita
+        if (e == 1){
+            direcao.scrollLeft = direcao.scrollLeft - 500;
+        }
+        // Mover pra direita
+        else if(e == 2){
+            direcao.scrollLeft = direcao.scrollLeft + 500;
+        };
+    }
+
+    // Media query 599px - 940px
+    if(larguraTela >= 599 && larguraTela <= 940){
+        // Mover pra direita
+        if (e == 1){
+            direcao.scrollLeft = direcao.scrollLeft - 620;
+        }
+        // Mover pra direita
+        else if(e == 2){
+            direcao.scrollLeft = direcao.scrollLeft + 620;
+        };
+    }
+}
+
+// Carrossel Empreendimentos
+function direcaoServicosEmpreendimentos(e){
+    // Variável para direção dos cards do carrossel
+    var direcao = document.getElementById("contentSlideEmpreendimentos");
+
+
+    // Media query ... - 398px
+    if(larguraTela <= 398){
+        // Mover pra direita
+        if (e == 1){
+            direcao.scrollLeft = direcao.scrollLeft - 280;
+        }
+        // Mover pra direita
+        else if(e == 2){
+            direcao.scrollLeft = direcao.scrollLeft + 280;
+        };
+    }
+
+    // Media query 390px - 458px
+    if(larguraTela >= 390 && larguraTela <= 458){
         // Mover pra direita
         if (e == 1){
             direcao.scrollLeft = direcao.scrollLeft - 280;
