9d0fee9780ea0c99f963485dd283d063d38ed061
Caio Montenegro <caiombmonteiro@gmail.com>
2025-03-27 18:47:02 -0300

    Criação do modal do formulário
    - Precisei mexer no base css porque ao tirar o link do btn Solicitar Orçamento que fica na nav o estilo se sumiu;
    - Na home, se o user tiver rolado pra baixo do site e clicar na logo que fica na navbar, o user será levado para a parte de cima do site.

---
+++ JS/modalFormulario.js
@@ -0,0 +1,12 @@
+// Variáveis
+var caixaFormulario = document.querySelector(".formulario");
+
+// Mostrar modal
+function mostrarModal(){
+    caixaFormulario.style.display = "block";
+}
+
+// Ocultar modal
+function ocultarModal(){
+    caixaFormulario.style.display = "none";
+}
